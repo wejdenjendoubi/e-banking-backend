03c33f918b767c163bf90220dae40d13b1dd0a56
wejdenjendoubi <jandoubiwijden@gmail.com>
2025-09-18 21:02:44 +0200

    Mise à jour du projet

--- ebankig/src/main/java/com/example/ebankig/model/Mondat.java
+++ ebankig/src/main/java/com/example/ebankig/model/Mondat.java
@@ -1,14 +1,14 @@
 package com.example.ebankig.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+
 import java.util.Objects;
+
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 
 @Entity
